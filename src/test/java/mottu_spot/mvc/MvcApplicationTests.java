1f601f09e00164c9f99e0a1a022eb40d3bb6e96f
MUKINH4 <samuelmaragato3@gmail.com>
2025-11-04 18:24:49 -0300

    fix

--- src/test/java/mottu_spot/mvc/MvcApplicationTests.java
+++ src/test/java/mottu_spot/mvc/MvcApplicationTests.java
@@ -2,10 +2,10 @@ package mottu_spot.mvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class MvcApplicationTests {
 
 	@Test
