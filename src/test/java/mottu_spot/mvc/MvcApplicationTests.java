97d2edeb16c12c551f649b0ff67d2c149a58c6b4
MUKINH4 <samuelmaragato3@gmail.com>
2025-11-04 17:56:59 -0300

    app.prop pra tests

--- src/test/java/mottu_spot/mvc/MvcApplicationTests.java
+++ src/test/java/mottu_spot/mvc/MvcApplicationTests.java
@@ -2,12 +2,15 @@ package mottu_spot.mvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MvcApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test passes if Spring context loads successfully
 	}
 
 }
